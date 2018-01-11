c0440ea269d522bb8a8d4dc3008baab492521b9f
Slavi Marinov <smarinov@ahl.com>
2018-01-11 14:50:40 +0000

    Bump up version in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except (IOError, ImportError, OSError):
 
 setup(
     name='pynorama',
-    version='0.3.0',
+    version='0.4.1',
     author="Man AHL Technology",
     author_email="ManAHLTech@ahl.com",
     description='Natural Language Processing Visualization Tool',
